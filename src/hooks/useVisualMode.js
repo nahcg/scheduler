275e9584e8e9364ef25688f626184da958d78b0c
nahcg <glor.chan@mail.utoronto.ca>
2023-09-16 14:08:09 -0400

    add transition functions and logic

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, result = false) {
+    if (!result) {
+      setHistory((prev) => [...prev, newMode]);
+    }
+
+    setHistory((prev) => [...prev.slice(0, -1), newMode]);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
+    }
+  }
+
+  return { mode: history[history.length - 1], transition, back };
+}
